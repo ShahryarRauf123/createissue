eddb358d4f33090f984cf4f982a4399718f10078
ShahryarRauf123 <shahryarrauf697@gmail.com>
2024-01-23 23:36:15 +0500

    FIXED

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
+// import SimpleMDE from "react-simplemde-editor";
 import {useForm, Controller} from 'react-hook-form'
 import "easymde/dist/easymde.min.css";
 import {TextField, Button, Callout, Text} from '@radix-ui/themes'
@@ -13,7 +14,9 @@ import { z } from 'zod';
 import ErrorMessage from "@/app/components/ErrorMessage";
 import { Spinner } from "@/app/components/Spinner";
 import { useEffect } from "react";
-import dynamic from "next/dynamic";
+
+
+const  SimpleMDE  = dynamic(() => import('react-simplemde-editor'), {   ssr: false })
 
 
 
